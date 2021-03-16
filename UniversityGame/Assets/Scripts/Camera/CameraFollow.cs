2b04ad817001df44ea242c72143503da2e510664
Its-Just-Lowe <80772409+Its-Just-Lowe@users.noreply.github.com>
2021-03-16 15:32:14 +0000

    Camera follow

---
+++ UniversityGame/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+
+    public bool usesBounds = false;
+    public Vector2 minBounds;
+    public Vector2 maxBounds;
+
+    void FixedUpdate()
+    {
+        if (target != null)
+        {
+            Vector3 desiredPosition = target.position + offset;
+
+            if (usesBounds)
+            {
+                desiredPosition.x = Mathf.Clamp(desiredPosition.x, minBounds.x, maxBounds.x);
+                desiredPosition.y = Mathf.Clamp(desiredPosition.y, minBounds.y, maxBounds.y);
+            }
+
+            Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+            transform.position = smoothedPos;
+        }
+    }
+}
